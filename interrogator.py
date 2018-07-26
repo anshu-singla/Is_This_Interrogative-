aef25bd940a18c4306e5047e6e2cbeb8de0643c2
Anshu <anshusingla10121996@gmail.com>
2018-07-26 23:02:21 +0530

    Stopwords in english in NLTK corpus

--- interrogator.py
+++ interrogator.py
@@ -1,10 +1,17 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 example_text = "Hello there Mr. Singh, how are you doing today? The weather is great and Python is pretty good itself. The sky is bluish-red and you should not eat anything not edible!"
 
 
-print(sent_tokenize(example_text))
+#print(sent_tokenize(example_text))
 
-print("----------------------------")
+print("----------------------------Tokenizing")
 
-print(word_tokenize(example_text))
+words = word_tokenize(example_text)
+print(words)
+
+print("----------------------------Stopwords")
+
+stop_words = set(stopwords.words("english"))
+print(stop_words)
