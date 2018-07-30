5e3c4190e5298f7ee6ccb952951f81c427b275b4
Anshu <anshusingla10121996@gmail.com>
2018-07-30 23:05:29 +0530

    Phase 2: Implementing the feature set by the correct way and using naive bayes as a classifier using feature Sets -> All first word begin with Wh_Word and sentence end with '?'

--- classify.py
+++ classify.py
@@ -1,8 +1,6 @@
 import nltk
 import random
-from nltk import ngrams
 from nltk.corpus import qc
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 
@@ -16,18 +14,18 @@ Sample_Questions = ["what is the weather like","where are we today","why did you
                     "what is a dinosour","what do i do in an hour","why do we have to leave at 6.00", "When is the apointment","where did you go","why did you do that","how did he win","why won’t you help me",
                     "when did he find you","how do you get it","who does all the shipping","where do you buy stuff","why don’t you just find it in the target","why don't you buy stuff at target","where did you say it was",
                     "when did he grab the phone","what happened at seven am","did you take my phone","do you like me","do you know what happened yesterday","did it break when it dropped","does it hurt everyday",
-                    "does the car break down often","can you drive me home","where did you find me"
+                    "does the car break down often","can you drive me home","where did you find me", "Are you okay with this", "Will they ever leave",
                     "can it fly from here to target","could you find it for me, Are you coming with me or not"]
 Sample_Answer = ["The asnwer is so simple", "This looks like an easy thing to do.", "This must be doable.", "You know you should do what he is telling you to.", "This is an orange", "That was a train.", "Where there is a will, there is a way"]               
 
 wh_words_tags = ["WDT", "WP", "WP$", "WRB"]
 
-stop_words = open("Stopwords.txt")
-stop_words = stop_words.read()
-
 qc_train = qc.tuples("train.txt")
 traindocuments = [x[1] for x in qc_train]
 traindocuments = traindocuments[:250]
+
+trainDec = open("RawTestingDataDeclarative.txt").read()
+trainDec = nltk.sent_tokenize(trainDec)
 
 # print(traindocuments)
 
@@ -35,10 +33,7 @@ qc_testInt = qc.tuples("test.txt")
 testdocuments = [x[1] for x in qc_testInt]
 testdocuments = testdocuments + Sample_Questions
 
-trainDec = open("RawTestingDataDeclarative.txt").read()
-trainDec = nltk.sent_tokenize(trainDec)
-
-testDec = Sample_Questions
+testDec = Sample_Answer
 
 # random.shuffle(testdocuments)
 
@@ -46,50 +41,57 @@ testDec = Sample_Questions
 
 # print(documents)
 
-def findFeaturesOneGram(documents, isInterrogative):
-	features = {}
+def ifTagExistsOnFirst(featureOfAParticularSentence, firstTag, tag):
+	if firstTag == tag:
+		featureOfAParticularSentence[tag] = True
+	else:
+		featureOfAParticularSentence[tag] = False
+
+def findFeaturesOneGram(documents, categoryDefined):
+	features = []
 	for sentence in documents:
+		featureOfAParticularSentence = {}
 		words = nltk.word_tokenize(sentence)
 		tagged = nltk.pos_tag(words)
 		firstWord = tagged[0][0]
 		firstTag = tagged[0][1]
-		tag = firstTag
-		if tag == "WDT" or tag == "WP" or tag == "WP$" or tag == "WRB":
-			features[tag] = isInterrogative
-		else:
-			features[tag] = not isInterrogative	
 
+		# 1st Feature
+		ifTagExistsOnFirst(featureOfAParticularSentence, firstTag, "WDT")
+
+		# 2nd Feature
+		ifTagExistsOnFirst(featureOfAParticularSentence, firstTag, "WP")
+
+		# 3rd Feature
+		ifTagExistsOnFirst(featureOfAParticularSentence, firstTag, "WP$")
+
+		# 4th Feature
+		ifTagExistsOnFirst(featureOfAParticularSentence, firstTag, "WRB")
+
+		# 5th Feature
 		lastWord = tagged[-1][0]
 		lastTag = tagged[-1][1]
-		if lastWord == "?":
-			features[lastWord] = isInterrogative
+		questionSign = "?"
+		if lastWord == questionSign:
+			featureOfAParticularSentence[questionSign] = True
 		else:
-			features[lastWord] = not isInterrogative
+			featureOfAParticularSentence[questionSign] = False
+		
 
+		myDict = (featureOfAParticularSentence, categoryDefined)
+		features.append(myDict)
 	return features
 
 # findFeatures(documents)
-trainfeaturesets = []
-trainfeaturesets.append((findFeaturesOneGram(traindocuments, True), categoryInterrogative))
-trainfeaturesets.append((findFeaturesOneGram(trainDec, False), categoryDeclarative))
+trainfeaturesets = findFeaturesOneGram(traindocuments, categoryInterrogative) + findFeaturesOneGram(trainDec, categoryDeclarative)
+random.shuffle(trainfeaturesets)
 # print(trainfeaturesets)
 
-testfeaturesets = []
-testfeaturesets.append((findFeaturesOneGram(testdocuments, True), categoryInterrogative))
-testfeaturesets.append((findFeaturesOneGram(testDec, False), categoryDeclarative))
+testfeaturesets = findFeaturesOneGram(testdocuments, categoryInterrogative) + findFeaturesOneGram(testDec, categoryDeclarative)
+random.shuffle(testfeaturesets)
 # print(testfeaturesets)
 
 classifier = nltk.NaiveBayesClassifier.train(trainfeaturesets)
 print("Naive Bayes Classifier Algo Accuracy Percent:", (nltk.classify.accuracy(classifier, testfeaturesets))*100)
 
-# def findFeaturesBiGram(documents, isInterrogative, n=2):
-# 	features = {}
-# 	for sentence in documents:
-# 		words = nltk.word_tokenize(sentence)
-# 		tagged = nltk.pos_tag(words)
-# 		ngram_vocab = ngrams(tagged, n)
-# 		for ng in ngram_vocab:
-# 			ngk
-# 		if tag == "WDT" or tag == "WP" or tag == "WP$" or tag == "WRB":
-# 	return features
 
